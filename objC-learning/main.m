c1898e62473321bf7e0b148400cfad4382dbea0c
Gregorio Setti <gregorio.setti@gmail.com>
2014-07-26 19:34:52 +0200

    Init commit

---
+++ objC-learning/main.m
@@ -0,0 +1,12 @@
+//
+// main.m
+// prog1 //
+// Created by Steve Kochan on 10/16/13.
+// Copyright (c) 2013 ClassroomM. All rights reserved. //
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog (@"Hello World!"); }
+    return 0;
+}

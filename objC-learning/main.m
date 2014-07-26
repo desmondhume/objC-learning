b66d3afda4d8850b5be4634d2563af3e9cd88224
Gregorio Setti <gregorio.setti@gmail.com>
2014-07-26 19:38:16 +0200

    Cleanup

--- objC-learning/main.m
+++ objC-learning/main.m
@@ -1,8 +1,6 @@
 //
 // main.m
-// prog1 //
-// Created by Steve Kochan on 10/16/13.
-// Copyright (c) 2013 ClassroomM. All rights reserved. //
+
 #import <Foundation/Foundation.h>
 
 int main (int argc, const char * argv[]) {
